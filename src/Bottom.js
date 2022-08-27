89ee363a2ccb71dd2e0785a4de505a2dc958b1e6
Akoruu.Dev <akoruu.dev@gmail.com>
2022-08-27 17:49:01 -0300

    FEAT [Bottom Status]: Create a function for end game status from App

--- src/Bottom.js
+++ src/Bottom.js
@@ -1,24 +1,30 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function Bottom({Answered, allQuestions, icons}) {
     const [message, setMessage] = useState(false)
     const [messageGood, setMessageGood] = useState(true)
 
-    if(Answered === allQuestions && icons != 0) {
-        setMessage(true);
-        console.log(icons) 
-    }
+    useEffect(() => {
+        if(Answered === allQuestions && icons !== 0) {
+            setMessage(true);
+            icons.find(icon => {
+                if(icon === 'close-circle') {
+                    setMessageGood(false)
+                }
+            });
+        }
+    }, [Answered])
 
     return(
         <div className="bottom">
             {message ?
                 messageGood ?
                 <div className="bottom-message">
-                    <p>Parabéns!</p>
+                    <p>&#129395; Parabéns!</p>
                     <p>Você não esqueceu de nenhum flashcard!</p>
                 </div>
                 : <div className="bottom-message">
-                    <p>Putz...</p>
+                    <p>&#128549; Putz...</p>
                     <p>Ainda faltam alguns...</p>
                     <p>Mas não desanime!</p>
                 </div> : ""
